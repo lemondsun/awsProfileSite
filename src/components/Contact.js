6b5e50d409050f5014777495f89692e37042dcad
Jason Mullings <wjasonmullings@gmail.com>
2020-05-14 18:33:55 -0400

    added resume link and contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,7 +3,24 @@ import React from 'react'
 export default function Contact() {
   return (
     <div>
-      
+    <div id="contact-form-area">
+    <h1 id="contact-header" >Contact Me</h1>
+    <form id="contact-form" action="https://formspree.io/xpzqaewg" method="POST">
+      <label>
+        Your name:
+        <input className="form-input" type="text" name="name" ></input>
+        </label>
+  <label>
+  Your email:
+  <input type="text" name="_replyto"></input>
+</label>
+        <label>
+      Your message:
+      <textarea name="message"></textarea>
+    </label>
+  <button id="contact-form-button" type="submit" value="Send">send</button>
+</form>
+  </div>
     </div>
   )
 }
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -7,7 +7,7 @@ export default function Menu() {
       <Link to="/techstack"><h1 class='menu-item'><span class='method'>.techStack</span><span class="parenthesis">( )</span></h1></Link>
       <Link to='/projects'><h1 class='menu-item'><span class='method'>.projects</span><span class="parenthesis">( )</span></h1></Link>
       <Link to='/contact'><h1 class='menu-item'><span class='method'>.contact</span><span class="parenthesis">( )</span></h1></Link>
-      <Link to='resume'><h1 class='menu-item'><span class='method'>.resume</span><span class="parenthesis">( )</span></h1></Link>
+      <a href='https://docdro.id/UVBCUo8' target="_blank" ><h1 class='menu-item'><span class='method'>.resume</span><span class="parenthesis">( )</span></h1></a>
     </div>
   )
 }
