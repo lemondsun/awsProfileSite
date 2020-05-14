247b35ab89df0567386c823cad7910ec2279af99
Jason Mullings <wjasonmullings@gmail.com>
2020-05-14 18:18:56 -0400

    basic set up for projects page'

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,8 +2,105 @@ import React from 'react'
 
 export default function Projects() {
   return (
-    <div>
-      
+    <div id='projects-div'>
+    <h1 class='commented-title'>Projects:</h1>
+
+    <div id="project-row">
+      <div className="project-area">
+        <a href="http://wisewords.surge.sh/" target="_blank">
+          <h3 className="projects-titles">Wise-words</h3>
+        </a>
+        <p>
+          A web app designed to answer simple questions with the use of the
+          React framework and an external API.
+        </p>
+        <img
+          className="project-images"
+          src="https://media.giphy.com/media/duLrxdbtlDABbsRZo8/giphy.gif"
+        />
+        <hr></hr>
+        <img
+          className="project-languages-images"
+          src="https://www.logolynx.com/images/logolynx/e1/e12c387bd8ebccf18a4741b4b83a6d71.png"
+        />
+        <img
+          className="project-languages-images"
+          src="https://cdn.worldvectorlogo.com/logos/javascript-1.svg"
+        />
+        <hr className="projects-bottom-divider"></hr>
+        <a href="https://github.com/lemondsun/Wise-Words" target="_blank">
+          <img
+            className="github-link"
+            src="https://image.flaticon.com/icons/png/512/25/25231.png"
+          />
+        </a>
+      </div>
+      <div className="project-area">
+        <a href="http://chuckedout.surge.sh/" target="_blank">
+          <h3 className="projects-titles">Chucked Out</h3>
+        </a>
+        <p>
+          A fun dive into the world of API's. Made with vanilla Javascript
+          and two seperate API's.
+        </p>
+        <img
+          className="project-images"
+          src="https://i.imgur.com/TpbW1qN.jpg"
+        />
+        <hr></hr>
+        <img
+          className="project-languages-images"
+          src="https://cdn.worldvectorlogo.com/logos/javascript-1.svg"
+        />
+        <img
+          className="project-languages-images"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/512px-HTML5_logo_and_wordmark.svg.png"
+        />
+        <img
+          className="project-languages-images"
+          src="https://upload.wikimedia.org/wikipedia/commons/d/d5/CSS3_logo_and_wordmark.svg"
+        />
+        <hr className="projects-bottom-divider"></hr>
+        <a
+          href="https://github.com/lemondsun/could-chuck-game"
+          target="_blank"
+        >
+          <img
+            className="github-link"
+            src="https://image.flaticon.com/icons/png/512/25/25231.png"
+          />
+        </a>
+      </div>
+      <div className="project-area">
+        <a href="http://tartinenyc.surge.sh/" target="_blank">
+          <h3 className="projects-titles">Tartine</h3>
+        </a>
+        <p>
+          A remodeled website developed with a team of UX designers and
+          other Software Engineers.
+        </p>
+        <img
+          className="project-images"
+          src="https://media.giphy.com/media/mBptyFCBPUSMBewxk2/giphy.gif"
+        />
+        <hr></hr>
+        <img
+          className="project-languages-images"
+          src="https://www.logolynx.com/images/logolynx/e1/e12c387bd8ebccf18a4741b4b83a6d71.png"
+        />
+        <img
+          className="project-languages-images"
+          src="https://cdn.worldvectorlogo.com/logos/javascript-1.svg"
+        />
+        <hr className="projects-bottom-divider"></hr>
+        <a href="https://tartinenyc.surge.sh" target="_blank">
+          <img
+            className="github-link"
+            src="https://image.flaticon.com/icons/png/512/25/25231.png"
+          />
+        </a>
+      </div>
+    </div>
     </div>
   )
 }
--- src/components/TechStack.js
+++ src/components/TechStack.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import Menu from '../components/Menu'
 
 export default function () {
   return (
     
     <div>
+      <Menu/>
       <div id='about-me-div'>
-        <h1>//About Me</h1>
+        <h1 class='commented-title'>//About Me</h1>
         <p id="brand-statement">
         I am a flexible and energetic software engineer with a passion to help
             create the engines that will drive the world of tomorrow. My values for
@@ -17,12 +19,55 @@ export default function () {
       </p>
       </div>
       <div className="parallax"></div>
-      <div>
-        <h1>//Tech Stack</h1>
+      <div id='tech-stack-div'>
+        <h1 class='commented-title'>//Tech Stack</h1>
+        <div id='languages-images-box'>
+      <img
+        className="languages-images"
+            src="https://www.logolynx.com/images/logolynx/e1/e12c387bd8ebccf18a4741b4b83a6d71.png"
+            alt="react logo"
+          />
+          
+      <img
+        className="languages-images"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/512px-HTML5_logo_and_wordmark.svg.png"
+            alt="html logo"
+      />
+      <img
+        className="languages-images"
+        src="https://upload.wikimedia.org/wikipedia/commons/d/d5/CSS3_logo_and_wordmark.svg"
+        alt="css logo"
+          />
+      <img
+        className="languages-images"
+        src="https://cdn.worldvectorlogo.com/logos/javascript-1.svg"
+        alt="javascript logo"
+          />
+      <img
+        className="languages-images"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Ruby_logo.svg/1024px-Ruby_logo.svg.png"
+        alt="ruby logo"
+          />
+      <img
+        className="languages-images"
+        src="https://cdn.freebiesupply.com/logos/thumbs/2x/rails-1-logo.png"
+        alt="rails logo"
+          />
+      <img
+        className="languages-images"
+        src="https://cdn.iconscout.com/icon/free/png-256/postgresql-11-1175122.png"
+        alt="postgreSQL logo"
+          />
+      <img
+        className="languages-images"
+        src="https://seeklogo.com/images/P/python-logo-C50EED1930-seeklogo.com.png"
+        alt="python logo"
+          />
+      </div>
       </div>
       <div className="parallax"></div>
-      <div>
-      <h1>.return(toTop)</h1>
+      <div id='bottom-div'>
+      <h1 class='to-top-button'><span class='method'>.return</span><span class='parenthesis'>(<span class='argument'>toTop</span>)</span></h1>
       </div>
       </div>
      
