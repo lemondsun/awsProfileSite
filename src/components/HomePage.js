fccab2e2f91374a3e5c6a2d5f1f3a812c9635b25
Jason Mullings <wjasonmullings@gmail.com>
2020-05-14 14:03:14 -0400

    finish basic title

---
+++ src/components/HomePage.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function HomePage() {
+  return (
+    <div id="home-page-div">
+      <div id='titled-header'>
+      <h1 id="const-title">const</h1>
+      <h1 id="name-title">Jason  Mullings</h1>
+      <h1 id="programmer-title">
+      <span id="new">= new </span>
+        <span class='method'>Programmer</span>
+          <span class="parenthesis">( )</span>
+      </h1>
+      </div>
+    </div>
+  )
+}
---
+++ src/components/Menu.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Menu() {
+  return (
+    <div id='menu-bar'>
+      <h1>techStack</h1>
+      <h1>projects</h1>
+      <h1>contact</h1>
+      <h1>resume</h1>
+    </div>
+  )
+}
