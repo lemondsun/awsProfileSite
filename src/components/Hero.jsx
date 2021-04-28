f72166db36412229a79f1a7f3d50a9db950056a2
Jason Mullings <wjasonmullings@gmail.com>
2021-04-28 12:30:20 -0400

    media queries

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,8 +3,10 @@ import React from 'react'
 export default function Hero() {
   return (
     <div class='hero-section'>
+      <div class='hero-name'>
       <p class='heading-primary first-name'>JASON</p>
       <p class='heading-primary last-name'>MULLINGS</p>
+      </div>
       <div class='hero-menu'>
         <a  class='heading-secondary' href='#about'>
           <p>THE PROGRAMMER</p>
@@ -16,6 +18,17 @@ export default function Hero() {
           <p>CONTACT</p>
           </a>
       </div>
+      {/*<div class='hero-menu-long'>
+      <a  class='heading-secondary' href='#about'>
+        <p>THE PROGRAMMER</p>
+      </a>
+      <a class='heading-secondary' href='#work'>
+      <p>MY WORK</p>
+      </a>
+      <a class='heading-secondary' href='#contact'>
+        <p>CONTACT</p>
+        </a>
+  </div>*/}
     </div>
   )
 }
